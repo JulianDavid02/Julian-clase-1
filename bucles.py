de75907e097bcd59ee408825eb00f44f8e37e08f
JulianDavid02 <149254857+JulianDavid02@users.noreply.github.com>
2023-11-11 16:02:24 -0500

    Add files via upload

--- bucles.py
+++ bucles.py
@@ -36,11 +36,106 @@
 
 #EJERCICIO 1
 
-num = int(input('ingrese numero'))
+# num = int(input('ingrese numero'))
 
-suma_pares = 0 
+# suma_pares = 0 
 
-for i in range(1, num + 1):
-    if i % 2 == 0:
-     suma_pares += i
-    print(f'Suma de numeros pares desde 1 hasta {num} es: {suma_pares}')
+# for i in range(1, num + 1):
+#     if i % 2 == 0:
+#      suma_pares += i
+#     print(f'Suma de numeros pares desde 1 hasta {num} es: {suma_pares}')
+    
+    # # Ejercicio 3 -----------------------------------
+
+# lineas = 5
+
+# for x in range (lineas + 1):
+#     print('*' * x)
+
+
+# # --------------------------------------------------------------------------------------------------------------------------------
+
+# #condicionales y bucles
+
+# #ejercicio 1---------------------
+
+
+# num = int(input("ingrese numero"))
+# suma_pares = 0
+# for x in range(1, num + 1):
+#     if x % 2 == 0: 
+#         suma_pares += x
+#         print(f" suma de numeros pares desde 1 hasta {num} es: {suma_pares}")
+
+
+# #ejercicio 2 ----------------------
+
+# palabra = input("Ingrese una palabra: ")
+
+# contador_vocales = 0
+
+# palabra = palabra.lower()
+
+# indice = 0
+
+# while indice < len(palabra):
+#     caracter = palabra[indice]
+#     if caracter in "aeiou":
+#         contador_vocales += 1
+#     indice += 1
+
+# print(f"La cantidad de vocales en la palabra es: {contador_vocales}")
+
+
+# # Ejercicio 3 -------------------
+
+# numero = int(input("Ingrese un número entero positivo mayor que 1: "))
+
+# es_primo = True
+
+# for i in range(2, int(numero**0.5) + 1):
+#     if numero % i == 0:
+#         es_primo = False
+#         break
+
+
+# if es_primo:
+#     print(f"{numero} es un número primo.")
+# else:
+#     print(f"{numero} no es un número primo.")
+
+# # Ejercicio 4 ------------------------------------------------------------------------------------------------
+
+# import random
+
+# numero_secreto = random.randint(1, 100)
+
+# print("¡Bienvenido al juego de adivinanzas!")
+# print("Intenta adivinar el número secreto entre 1 y 100.")
+
+# intentos = 0
+
+# while True:
+#     intento = int(input("Ingresa tu intento: "))
+    
+#     intentos += 1
+
+#     if intento == numero_secreto:
+#         print(f"¡Felicidades! ¡Adivinaste el número secreto {numero_secreto} en {intentos} intentos!")
+#         break
+#     elif intento < numero_secreto:
+#         print("Demasiado bajo. Intenta de nuevo.")
+#     else:
+#         print("Demasiado alto. Intenta de nuevo.")
+
+# # Ejercicio 5 -------------------------------------------------------------------------------------------
+
+numero = int(input("Ingrese un número: "))
+
+
+if numero % 2 == 0:
+    for i in range(numero):
+        print('*', end=' ')
+else:
+    for i in range(numero):
+        print('* ' * numero)
