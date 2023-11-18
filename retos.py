d15efc5378e512a5c488e4a1bbc48d7f486d612c
JulianDavid02 <149254857+JulianDavid02@users.noreply.github.com>
2023-11-18 12:07:03 -0500

    Add files via upload

---
+++ retos.py
@@ -0,0 +1,22 @@
+#EJERCICIO 5 CONDICIONALES
+
+# def evaluar_salario(salario):
+#     if salario <= 10000:
+#         print('No paga impuestos')
+#     elif salario > 10000 and salario < 50000:
+#         print('Paga 10% impuestos')  
+#     elif salario > 5000 and salario < 100000:
+#         print('Paga 20% impuestos')  
+#     else:
+#         print('Paga 30% de impuestos')   
+         
+# evaluar_salario(10000)      
+
+
+#EJERCICIO 10 BUCLES Patron de asteriscos
+
+def cant_asteriscos(filas):
+    for i in range (1, filas + 1):
+        print('* ' * i)
+
+cant_asteriscos(5)           
